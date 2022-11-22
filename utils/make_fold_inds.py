7e1867c09db56645304a1677e6e302229c353404
Anton Korikov <anton.korikov@mail.utoronto.ca>
2022-11-22 12:26:14 -0500

    work on prompt tuning

--- utils/make_fold_inds.py
+++ utils/make_fold_inds.py
@@ -15,7 +15,7 @@ import random
 import math
 import json
 
-def make_fold_inds(k = 1, train_frac = 0.1, val_frac = 0.5, seed = 0, data_path = "../data/500QA.json", write_path = "../data/fold_inds.txt"):
+def make_fold_inds(K = 1, train_frac = 0.1, val_frac = 0.5, seed = 0, data_path = "../data/500QA.json", write_path = "../data/fold_inds.json"):
 	random.seed(seed)
 
 	all_fold_inds = []
@@ -26,14 +26,15 @@ def make_fold_inds(k = 1, train_frac = 0.1, val_frac = 0.5, seed = 0, data_path 
 
 	n = len(data)
 
-	#make list of indicies
+	#make list of ordered indicies
 	ordered_inds = list(range(n))
 	
-	for i in range(k):
+	for i in range(K):
 		#make lists for train_inds, val_inds, test_inds and aggregate into kth_fold_inds
 
 		shuffled_inds = random.sample(ordered_inds,n)
 
+		#slice based on train/val/test fractions
 		train_inds = shuffled_inds[0:math.ceil(n*train_frac)]
 		val_inds = shuffled_inds[math.ceil(n*train_frac):(math.ceil(n*train_frac) + math.ceil(n*val_frac))]
 		test_inds = shuffled_inds[(math.ceil(n*train_frac) + math.ceil(n*val_frac)):]
@@ -41,22 +42,21 @@ def make_fold_inds(k = 1, train_frac = 0.1, val_frac = 0.5, seed = 0, data_path 
 		kth_fold_inds = [train_inds,val_inds,test_inds]
 		all_fold_inds.append(kth_fold_inds)
 
+	#write
 	with open(write_path, 'w') as f:
-		for i in range(k):
-			f.write(f"{all_fold_inds[i]}\n")
+		json.dump(all_fold_inds,f)
 
-	#return all_fold_inds
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-k', default = 1)
+	parser.add_argument('-K', default = 1)
 	parser.add_argument('-train_frac', default = 0.1)
 	parser.add_argument('-val_frac', default = 0.1)
 	parser.add_argument('-seed', default = 0)
 	parser.add_argument('-data_path', default = "../data/500QA.json")
-	parser.add_argument('-write_path', default = "../data/fold_inds.txt")
+	parser.add_argument('-write_path', default = "../data/fold_inds.json")
 
 	args = parser.parse_args()
 
-	make_fold_inds(k = int(args.k), train_frac = float(args.train_frac), val_frac = float(args.val_frac), seed = float(args.seed), data_path = args.data_path, write_path = args.write_path)
+	make_fold_inds(K = int(args.K), train_frac = float(args.train_frac), val_frac = float(args.val_frac), seed = float(args.seed), data_path = args.data_path, write_path = args.write_path)
 
