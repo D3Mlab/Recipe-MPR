b390a0ca9a89b62f465d57c75ea89e051c8cfc88
HaochenZ11 <haochen.zhang@gmail.com>
2023-02-05 14:17:55 -0500

    small fixes, add extracted aspect option to config

--- baselines/helper.py
+++ baselines/helper.py
@@ -1,26 +1,10 @@
 import json
-#from transformers import  AutoTokenizer, OPTForCausalLM, GPT2Tokenizer, GPT2LMHeadModel, OpenAIGPTLMHeadModel, OpenAIGPTTokenizer
 
-def load_config(config_path):
+def load_config(config_path='../config.json'):
 	with open(config_path) as cf:
 		config = json.load(cf)
-
-	with open(config['data_path']) as df:
-		all_data = json.load(df)
-
-	with open(config['folds_path'], 'r', encoding='utf-8') as f:
-		folds = json.load(f)
 	
-	return all_data, folds
-
-def get_model(model_name):
-	model_config = {
-		"facebook/opt-1.3b": [OPTForCausalLM, AutoTokenizer], 
-		"gpt2": [GPT2LMHeadModel, GPT2Tokenizer],
-		"openai-gpt": [OpenAIGPTLMHeadModel, OpenAIGPTTokenizer],
-		"microsoft/DialoGPT-large": [GPT2LMHeadModel, GPT2Tokenizer]
-	}
-	return model_config[model_name][0], model_config[model_name][1]
+	return config
 
 # aggregation function
 def aggregate(scores, fcn):
